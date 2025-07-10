4937bf2592c86125ff1c192fefa02378d0c38b32
Matte Noble <me@mattenoble.com>
2025-07-10 15:20:11 -0700

    Move ext tsdoc into interface file so LSPs pick it up

--- src/events/filesystem.ts
+++ src/events/filesystem.ts
@@ -6,12 +6,12 @@ import { Trigger } from '$lib/models';
 
 listen('filesystem/file-created', async (event: Event<FileCreatedEvent>) => {
     await Trigger.where({ event: 'filesystem', action: 'file_created' })
-        .map(t => t.app)
+        .mapBy('app')
         .awaitAll(async app => app.execute(event.payload));
 });
 
 listen('filesystem/file-updated', async (event: Event<FileUpdatedEvent>) => {
     await Trigger.where({ event: 'filesystem', action: 'file_updated' })
-        .map(t => t.app)
+        .mapBy('app')
         .awaitAll(async app => app.execute(event.payload));
 });
--- src/lib/ext.ts
+++ src/lib/ext.ts
@@ -8,44 +8,16 @@
  * `src/app.d.ts`.
  */
 
-/**
- * Return the Object without k/v pairs where the value is explicitly undefined.
- *
- * Typically you'll use this after mapping a bunch of values where some may be
- * undefined.
- *
- * @example
- * ```ts
- * const compacted = Object.compact({ a: 1, b: true, c: undefined });
- * assertEq(compacted, { a: 1, b: true });
- * ```
- */
+// Object
+
 Object.compact = function <T>(o: Obj): T {
     return Object.fromEntries(Object.entries(o).filter(([_, v]) => v !== undefined)) as T;
 };
 
-/**
- * Return the Object without specific k/v pairs, specified by key.
- *
- * @example
- * ```ts
- * const subobj = Object.without({a: 1, b: 2}, ['b']);
- * assertEq(subobj, {a: 1})
- * ```
- */
 Object.without = function <T>(o: Obj, keys: string[]): T {
     return Object.fromEntries(Object.entries(o).filter(([k, _]) => !keys.includes(k))) as T;
 };
 
-/**
- * Remove, and return, a value by key
- *
- * @example
- * ```ts
- * const value = Object.remove({a: 1, b: 2}, 'b');
- * assertEq(value, 2);
- * ```
- */
 Object.remove = function <T>(o: Obj, key: string): T | undefined {
     if (Object.hasOwn(o, key)) {
         const value = o[key];
@@ -54,23 +26,12 @@ Object.remove = function <T>(o: Obj, key: string): T | undefined {
     }
 };
 
-/**
- * Map over the entries of an object
- */
 Object.map = function <T extends Obj>(o: T, fn: (key: string, value: any) => any): T {
     return Object.fromEntries(Object.entries(o).map(([k, v]) => fn(k, v))) as T;
 };
 
-/**
- * Sort an array of objects by a specific key.
- *
- * @example
- * ```ts
- * const array = [{color: 'red'}, {color: 'blue'}];
- * const sorted = array.sortBy('color');
- * assertEq(sorted, [{color: 'blue'}, {color: 'red'}]);
- * ```
- */
+// Arrays
+
 Array.prototype.sortBy = function <T extends Obj>(this: T[], key: string): T[] {
     return [...this].sort((a, b) => {
         if (a[key] < b[key]) return -1;
@@ -79,15 +40,6 @@ Array.prototype.sortBy = function <T extends Obj>(this: T[], key: string): T[] {
     }) as T[];
 };
 
-/**
- * Find by a specific key, in an array of objects.
- *
- * @example
- * ```ts
- * const items = [{n: 1}, {n: 2}, {n: 3}];
- * assertEq(items.findBy('n', 2), {n: 2});
- * ```
- */
 Array.prototype.findBy = function <T extends Obj>(
     this: T[],
     key: string,
@@ -96,39 +48,18 @@ Array.prototype.findBy = function <T extends Obj>(
     return this.find(item => item[key] == value);
 };
 
-/**
- * Remove undefined's from an array
- *
- * @example
- * ```ts
- * const items = [1, undefined, 2, 3];
- * assertEq(items.compact(), [1, 2, 3]);
- * ```
- */
 Array.prototype.compact = function <T>(this: T[]): Exclude<T, undefined>[] {
     return this.filter(i => i !== undefined) as Exclude<T, undefined>[];
 };
 
-/**
- * Return the last element in the array
- */
 Array.prototype.last = function <T>(this: T[]): T {
     return this[this.length - 1];
 };
 
-/**
- * Map over an array with an async function and await them all.
- *
- * ```
- * const items = [1, 2, 3];
- *
- * await items.awaitAll(async i => await asyncthing(i));
- *
- * // is equivalent to
- *
- * await Promise.all(items.map(async i => await asyncthing(i)));
- * ```
- */
+Array.prototype.mapBy = function <T, K extends keyof T>(this: T[], key: K): T[K][] {
+    return this.map(item => item[key] as T[K]);
+};
+
 Array.prototype.awaitAll = async function <T>(
     this: T[],
     fn: (arg0: T) => Promise<any>
@@ -136,9 +67,8 @@ Array.prototype.awaitAll = async function <T>(
     return await Promise.all(this.map(fn));
 };
 
-/**
- * Truncate the text, suffixed by an ellipsis.
- */
+// String
+
 String.prototype.ellipsize = function (length: number = 25) {
     return this.substring(0, length - 3) + '...';
 };
