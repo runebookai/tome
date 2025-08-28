8b2c681eb0fc60314e076c99c1006d6a3d1ca721
Matte Noble <me@mattenoble.com>
2025-08-28 13:18:32 -0700

    Remove temporary Import test button

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { emit } from '@tauri-apps/api/event';
     import type { SvelteHTMLElements } from 'svelte/elements';
     import { twMerge } from 'tailwind-merge';
 
@@ -54,16 +53,6 @@
             <Svg name="Models" />
         </Link>
     </Tooltip>
-
-    <button
-        onclick={() =>
-            emit(
-                'apps/import',
-                'eyJuYW1lIjoiQ2FwcHkgSm9rZSIsInJlYWRtZSI6IiIsInRyaWdnZXIiOnsiZXZlbnQiOiJzY2hlZHVsZWQiLCJhY3Rpb24iOiJ0aWNrIiwiY29uZmlnIjp7InBlcmlvZCI6IjAgMTAgKiAqICoifX0sInN0ZXBzIjpbeyJtb2RlbCI6Im9wZW5haTpncHQtNC4xIiwicHJvbXB0IjoiVGVsbCBtZSBhIG5ldyBqb2tlLiJ9LHsibW9kZWwiOiJvcGVuYWk6Z3B0LTQuMSIsInByb21wdCI6Ik5vdyByZXdpdGUgaXQgdG8gYmUgYWJvdXQgY2FweWJhcmFzLiJ9XSwibWNwX3NlcnZlcnMiOlt7Im5hbWUiOiJzY3J5ZmFsbCIsImNvbW1hbmQiOiJucHgiLCJhcmdzIjpbIi15Iiwic2NyeWZhbGwtbWNwLXNlcnZlciJdLCJlbnYiOnt9fSx7Im5hbWUiOiJQbGF5d3JpZ2h0ICIsImNvbW1hbmQiOiJucHgiLCJhcmdzIjpbIi15IiwiQHBsYXl3cmlnaHQvbWNwQGxhdGVzdCJdLCJlbnYiOnsiQUxXQVlTX09OIjoidHJ1ZSIsIlBPUlQiOiIxMjM0NSJ9fV19'
-            )}
-    >
-        import
-    </button>
 
     <div class="grow"></div>
 
