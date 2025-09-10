68771066e42b7393edabcd558ad04d060a4625dd
Matte Noble <me@mattenoble.com>
2025-09-10 14:29:52 -0700

    Allow model selection on App Import

--- src/components/App/Transfer/Import/Info.svelte
+++ src/components/App/Transfer/Import/Info.svelte
@@ -4,8 +4,7 @@
 
     import Flex from '$components/Flex.svelte';
     import LabeledSection from '$components/Forms/LabeledSection.svelte';
-    import Icon from '$components/Icon.svelte';
-    import Tooltip from '$components/Tooltip.svelte';
+    import ModelSelect from '$components/ModelSelect.svelte';
     import type { SerializedApp } from '$lib/apps';
     import { Model } from '$lib/models';
 
@@ -15,12 +14,16 @@
 
     const { app }: Props = $props();
 
+    let model: Model = $state(Model.findByOrDefault({ id: app.steps[0].model }));
+
     function readableCron(period: string) {
         return cronstrue.toString(period);
     }
 
-    function modelMissing(id: string) {
-        return !Model.exists({ id });
+    async function setModel(model: Model) {
+        for (const step of app.steps) {
+            step.model = model.id as string;
+        }
     }
 </script>
 
@@ -43,26 +46,15 @@
         </LabeledSection>
     {/if}
 
+    <LabeledSection icon="Models" title="Model">
+        <ModelSelect selected={model} onselect={setModel} class="h-10" />
+    </LabeledSection>
+
     <LabeledSection icon="Chat" title="Prompts">
         <Flex class="w-full flex-col items-start">
             {#each app.steps as step, i (i)}
                 <Flex class="border-light mb-2 -ml-4 w-full rounded-sm border p-2">
                     <p class="ml-2 grow">{step.prompt}</p>
-
-                    {#if modelMissing(step.model)}
-                        <Tooltip text="You must install this model before importing the app.">
-                            <p
-                                class="bg-light text-red flex items-center rounded-sm p-1 px-2 text-xs font-light"
-                            >
-                                <Icon name="Warning" class="mr-2 h-3 w-3" />
-                                {step.model}
-                            </p>
-                        </Tooltip>
-                    {:else}
-                        <p class="bg-light text-medium rounded-sm p-1 px-2 text-xs font-light">
-                            {step.model}
-                        </p>
-                    {/if}
                 </Flex>
             {/each}
         </Flex>
